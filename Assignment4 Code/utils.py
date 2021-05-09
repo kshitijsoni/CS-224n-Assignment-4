90f8b736bb54f8fb918150c6178a06b7ab2c1ea1
Kshitij Soni <60361593+kshitijsoni@users.noreply.github.com>
2021-05-09 12:06:47 +0530

    Update utils.py

--- Assignment4 Code/utils.py
+++ Assignment4 Code/utils.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-CS224N 2018-19: Homework 4
-nmt.py: NMT Model
-Pencheng Yin <pcyin@cs.cmu.edu>
-Sahil Chopra <schopra8@stanford.edu>
-"""
 
 import math
 from typing import List
